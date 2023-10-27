89f977d343c9d1521245398dfcb966ff751853ca
YuDavidCao <yu.cao20041208@gmail.com>
2023-10-26 23:31:16 -0400

    fix for compile

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,8 @@ Future<void> main() async {
   settingBox = await Hive.openBox('setting');
   dailyProgressBox = await Hive.openBox('dailyProgress');
   totalStatisticBox = await Hive.openBox("totalStatisticBox");
-  settingBox.put("image tracking agreement", false);
-  settingBox.put("image classification description", false);
+  // settingBox.put("image tracking agreement", false);
+  // settingBox.put("image classification description", false);
   if (!settingBox.containsKey("first time initialization")) {
     settingBox.put("first time initialization", true);
     settingBox.put("image tracking agreement", false);
